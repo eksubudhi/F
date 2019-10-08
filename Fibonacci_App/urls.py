ed8da87563b4945752963cad86dc5046849a0f24
KIRAN <ekirankumar7@gmail.com>
2019-10-08 09:31:30 +0530

    initial commit

--- Fibonacci_App/urls.py
+++ Fibonacci_App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Fibonacci_App import views
 
 urlpatterns = [
-    path('index', views.Fibonacci),
+    path('', views.Fibonacci),
     path('Fibonacci1',views.Fibonacci1),
 
 ]
