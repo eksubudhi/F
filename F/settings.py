ac7acb4be0d67ecde15454cc543c05d6166a07c0
KIRAN <ekirankumar7@gmail.com>
2019-10-08 10:28:17 +0530

    initial commit

--- F/settings.py
+++ F/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.jaoi(BASE_DIR,'static')
 
 
 
@@ -121,3 +122,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+STATIC_DIR,
+]
