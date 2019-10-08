7928ad16b5cda33d34be639cb943b2073a9f365e
KIRAN <ekirankumar7@gmail.com>
2019-10-08 09:06:53 +0530

    initial commit

--- F/urls.py
+++ F/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('Fibonacci_App.urls')),
+    #path('admin/', admin.site.urls),
+    path('index',include('Fibonacci_App.urls')),
 ]
